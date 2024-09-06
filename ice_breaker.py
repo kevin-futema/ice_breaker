3a3e6a1b60d35b99adf4432f9698389ebff95543
Kevin Futema <kevinfutema@Kevins-MacBook-Air.local>
2024-09-06 01:29:09 -0300

    formatting

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,5 +1,5 @@
 from langchain.prompts.prompt import PromptTemplate
-from langchain_openai  import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
@@ -9,13 +9,15 @@ from third_parties.linkedin import scrape_linkedin_profile
 if __name__ == "__main__":
     load_dotenv()
     print("Hello LangChain!")
-    summary_template =  """
+    summary_template = """
         give the Linkedin information {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template = summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
 
     # llm = ChatOpenAI(temperature=0, name="gpt-4o-2024-08-06")
     llm = ChatOllama(model="mistral")
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,39 +1,38 @@
-import os 
+import os
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def scrape_linkedin_profile(linkedin_url: str, mock: bool = False):
     if mock:
         url = "https://gist.githubusercontent.com/kevin-futema/25dda6488a072959ed4831b77f2400e0/raw/2ee4691c288faa40826d507f016eca92bf2484c7/gistfile1.json"
-        response = requests.get(
-            url,
-            timeout=10 
-        )
+        response = requests.get(url, timeout=10)
     else:
         api_key = os.environ.get("PROXYCURL_API_KEY")
-        headers = {'Authorization': 'Bearer ' + api_key}
-        api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
-        params = {
-            'url': linkedin_url
-        }
-        response = requests.get(api_endpoint,
-                                params=params,
-                                headers=headers)
+        headers = {"Authorization": "Bearer " + api_key}
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        params = {"url": linkedin_url}
+        response = requests.get(api_endpoint, params=params, headers=headers)
     data = response.json()
 
     data = {
         k: v
-        for k,v in data.items()
-        if v not in [{},"",[],None] and k not in ["people_also_viewed","certifications"]
+        for k, v in data.items()
+        if v not in [{}, "", [], None]
+        and k not in ["people_also_viewed", "certifications"]
     }
     if data.get("groups"):
         for group_dict in data.get("groups"):
             group_dict.pop("profile_pic_url")
     return data
-        
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     print(
-        scrape_linkedin_profile(linkedin_url="https://www.linkedin.com/in/kevin-futema-646483186/",mock=True)
+        scrape_linkedin_profile(
+            linkedin_url="https://www.linkedin.com/in/kevin-futema-646483186/",
+            mock=True,
+        )
     )
